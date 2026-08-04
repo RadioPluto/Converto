1822d1f9dc9e8d7bc64473a73da8be00f340adc5
Vivek Kumar <vivekkmr433@gmail.com>
2026-08-04 16:12:32 +0530

    BAckground color blue

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ def main(page: ft.Page):
     #page dimensions
     page.width = 500
     page.height = 500
+    page.window.resizeable = True
+    background_container = ft.Container(
+        width = 450,
+        height = 450,
+        bgcolor = '#041955',
+        border_radius = 35,
+        blur = 10
+    )
+    page.add(background_container)
+    #Result
+    result = ft.Text(value = "0")
 
 
 ft.run(main)
